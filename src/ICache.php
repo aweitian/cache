6dd9b22651f71a7e0ec01bde17ad63aa7b281050
tiananwei <awei.tian@qq.com>
2017-05-20 17:25:43 +0800

    cache for redis and memcache

---
+++ src/ICache.php
@@ -0,0 +1,29 @@
+<?php
+namespace Tian;
+interface ICache
+{
+	/**
+	 * @param $name
+	 * @param $value
+	 * @param $expire
+	 *
+	 * @return mixed
+	 */
+	public function set($name, $value, $expire);
+	/**
+	 * @param $name
+	 *
+	 * @return mixed
+	 */
+	public function get($name);
+	/**
+	 * @param $name
+	 *
+	 * @return mixed
+	 */
+	public function del($name);
+	/**
+	 * @return mixed
+	 */
+	public function flush();
+}
